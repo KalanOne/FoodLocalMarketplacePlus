304f9bd20757d45af7008f571a1711a7875a1024
KalanOne <onepunchalan@gmail.com>
2023-11-22 08:27:46 -0600

    Pequeños cambios

--- src/services/proveedor.services.ts
+++ src/services/proveedor.services.ts
@@ -1,9 +1,15 @@
 import { db } from "../utils/db";
 import { encrypt, verify } from "../utils/bcrypt.handle";
 import jwt from "jsonwebtoken";
-import { Proveedor, ProveedorLogin, ProveedorUpdate } from "../interfaces/proveedor.interface";
+import {
+  Proveedor,
+  ProveedorLogin,
+  ProveedorUpdate,
+} from "../interfaces/proveedor.interface";
 
-export const insertProveedor = async (proveedor: Proveedor): Promise<Proveedor | null> => {
+export const insertProveedor = async (
+  proveedor: Proveedor
+): Promise<Proveedor | null> => {
   const password = proveedor.password;
 
   const hash = await encrypt(password);
@@ -32,7 +38,9 @@ export const insertProveedor = async (proveedor: Proveedor): Promise<Proveedor |
   return newProveedor;
 };
 
-export const updateProveedor = async (proveedor: ProveedorUpdate): Promise<Proveedor | null> => {
+export const updateProveedor = async (
+  proveedor: ProveedorUpdate
+): Promise<Proveedor | null> => {
   const response = await db.proveedor.update({
     where: {
       email: proveedor.idProveedor,
@@ -52,7 +60,9 @@ export const updateProveedor = async (proveedor: ProveedorUpdate): Promise<Prove
   return response;
 };
 
-export const getProveedor = async (email: string): Promise<Proveedor | null> => {
+export const getProveedor = async (
+  email: string
+): Promise<Proveedor | null> => {
   const response = await db.proveedor.findUnique({
     where: {
       email: email,
@@ -63,7 +73,17 @@ export const getProveedor = async (email: string): Promise<Proveedor | null> => 
 };
 
 export const getProveedorAll = async (): Promise<Proveedor[] | null> => {
-  const response = await db.proveedor.findMany();
+  // const response = await db.proveedor.findMany();
+  const response = await db.proveedor.findMany({
+    include: {
+      productos: {
+        include: {
+          resenas: true,
+        },
+      },
+      resenas: true,
+    },
+  });
 
   return response;
 };
@@ -88,14 +108,19 @@ export const getProveedorRestaurant = async (): Promise<Proveedor[] | null> => {
   return response;
 };
 
-export const getProveedorLogin = async (proveedor: ProveedorLogin): Promise<string | null> => {
+export const getProveedorLogin = async (
+  proveedor: ProveedorLogin
+): Promise<string | null> => {
   const response = await db.proveedor.findUnique({
     where: {
       email: proveedor.email,
     },
   });
 
-  const passwordsMatch: Boolean = await verify(proveedor.password, response?.password || "");
+  const passwordsMatch: Boolean = await verify(
+    proveedor.password,
+    response?.password || ""
+  );
 
   if (!passwordsMatch) {
     return "NO_MATCH";
@@ -106,7 +131,11 @@ export const getProveedorLogin = async (proveedor: ProveedorLogin): Promise<stri
     tipo: "Proveedor",
   };
 
-  const accessToken = jwt.sign(tokenData, process.env.ACCESS_TOKEN_SECRET || "", { expiresIn: "2w" });
+  const accessToken = jwt.sign(
+    tokenData,
+    process.env.ACCESS_TOKEN_SECRET || "",
+    { expiresIn: "2w" }
+  );
 
   return accessToken;
 };
